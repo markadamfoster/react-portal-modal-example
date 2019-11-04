698148e40237851bc8fae9421b4b1e96f0cb9a39
Mark Foster <foster154@gmail.com>
2019-11-04 08:26:29 -0700

    add initial modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Toggle from "./Toggle";
+import Modal from "./Modal";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Toggle>
+          {({ on, toggle }) => (
+            <Fragment>
+              <button onClick={toggle}>Login</button>
+              <Modal on={on} toggle={toggle}>
+                <h1>I'm in a modal!</h1>
+              </Modal>
+            </Fragment>
+          )}
+        </Toggle>
       </header>
     </div>
   );
